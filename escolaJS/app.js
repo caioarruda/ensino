5e743d689f313e4a7c3691ada5de75661749b13d
caio arruda <caioarruda@gmail.com>
2021-08-05 20:48:09 -0300

    Add escrever nome

--- escolaJS/app.js
+++ escolaJS/app.js
@@ -1,1 +1,12 @@
-console.log('hello world!')
+const { textoDoTeclado, numeroDoTeclado } = require('./helpers/teclado')
+
+const escreverNome = (nomePessoa) => {
+  console.log('Olá ' + nomePessoa)
+}
+
+const main = async () => {
+  const nome = await textoDoTeclado('nome: ')
+  escreverNome(nome)
+}
+
+main()
