0cca43ebfcf44a285f51070f1e8909a2115dee6f
caio arruda <caioarruda@gmail.com>
2021-08-11 21:40:13 -0300

    fix app.js

--- escolaJS/app.js
+++ escolaJS/app.js
@@ -46,32 +46,4 @@ const main = async () => {
   }
   process.exit()
 }
-
-const fs = require('fs')
-
-const escreverJSON = () => {
-  const cachorros = [
-    { nome: 'Rex', raca: 'Pitbull' },
-    {
-      nome: 'Dog',
-      raca: 'Pastor Alemão'
-    }
-  ]
-
-  const jsonContent = JSON.stringify(cachorros)
-  return jsonContent
-}
-
-const escreverArquivo = (caminho) => {
-  const conteudo = escreverJSON()
-  fs.writeFileSync(caminho, conteudo)
-
-  const arquivo = fs.readFileSync(caminho, 'utf8')
-  console.log(arquivo[0])
-  const dogs = JSON.parse(arquivo)
-  console.log(dogs[0])
-}
-
-escreverArquivo('bd/dogs.json')
-
-//main()
+main()
